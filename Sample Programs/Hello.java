6033b7ddf45497099c3f8a4a4dbdf5d0f76e6043
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2023-12-27 08:32:00 +0530

    ok

--- Sample Programs/Hello.java
+++ Sample Programs/Hello.java
@@ -1,5 +1,28 @@
-class Hello{
-    public static  void main(String args[]){
-        
+
+    class Hello { 
+  
+    
+    static void Fibonacci(int N) 
+    { 
+        int num1 = 0, num2 = 1; 
+         for(int i=0;i<N;i++) 
+             
+ 
+            System.out.print(num1 + " "); 
+  
+            
+            int num3 = num2 + num1; 
+            num1 = num2; 
+            num2 = num3; 
+         
+        } 
+    
+     public static void main(String args[]) 
+    { 
+        // Given Number N 
+        int N = 10; 
+  
+        // Function Call 
+        Fibonacci(N); 
+    } 
     }
-}
