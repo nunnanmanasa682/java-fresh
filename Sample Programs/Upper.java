d71edeed32cb41d7df09065a5fcbdc3e3313a446
nunnanmanasa682 <nunnanmanasa682@gmail.com>
2023-12-27 08:31:27 +0530

    ok

--- Sample Programs/Upper.java
+++ Sample Programs/Upper.java
@@ -1,4 +1,10 @@
+class Upper{
+public static void main(String args[]){
+    
 String txt = "Hello World";
 System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
 System.out.println(txt.toLowerCase());   // Outputs "hello world"
+}
 
+
+}
